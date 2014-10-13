cc53ab022d4c96bf47cc9e719c4043f969261272
Heesung Ahn <ahnh@mail.uoguelph.ca>
2014-10-13 00:34:52 -0400

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <style type="text/css">
+      html, body, #map-canvas { height: 90%; width: 90%; margin: 0; padding: 0;}
+    </style>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGHfbO0-f12Zl2IhbFRp6NFEvtyY1zgGE">
+    </script>
+    <script type="text/javascript">alert("hello");</script>
+  </head>
+  <body>
+	<div id="map-canvas"></div>
+  </body>
+</html>
